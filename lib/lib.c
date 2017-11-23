3b672dc7c0024a635261e0fb13ed5b40fd6c398d
Tianyi Cui <v-ticu@microsoft.com>
2017-11-23 16:15:27 +0800

    Finished pthread and fork.

--- lib/lib.c
+++ lib/lib.c
@@ -4,29 +4,36 @@
 #include "lib.h"
 #include <sys/ipc.h>
 #include <sys/shm.h>
-static int uniq_shared_id;
-static void* uniq_shared_base_addr;
-//0 is to monitor 1 is from monitor
-static metaqueue_data* metaqueue;
-static metaqueue_meta metaqueue_metadata[2];
+#include <pthread.h>
+typedef struct {
+    int uniq_shared_id;
+    void* uniq_shared_base_addr;
+    //0 is to monitor 1 is from monitor
+    metaqueue_data* metaqueue;
+    metaqueue_meta metaqueue_metadata[2];
+} thread_data_t;
+pthread_key_t pthread_key;
 void connect_monitor()
 {
     ctl_struc result;
     setup_sock_connect(&result);
-    uniq_shared_id = shmget(result.key, 2*sizeof(metaqueue_data), 0777);
+    thread_data_t *data = (thread_data_t *)malloc(sizeof(thread_data_t));
+    pthread_setspecific(pthread_key, (void *)data);
+    data->uniq_shared_id = shmget(result.key, 2*sizeof(metaqueue_data), 0777);
     //printf("%d\n", uniq_shared_id);
-    if (uniq_shared_id == -1)
+    if (data->uniq_shared_id == -1)
         FATAL("Failed to open the shared memory, errno: %d", errno);
-    uniq_shared_base_addr = shmat(uniq_shared_id, NULL, 0);
-    if (uniq_shared_base_addr == (void*)-1)
+    data->uniq_shared_base_addr = shmat(data->uniq_shared_id, NULL, 0);
+    if (data->uniq_shared_base_addr == (void*)-1)
         FATAL("Failed to attach the shared memory, err: %s", strerror(errno));
-    metaqueue = (metaqueue_data*) uniq_shared_base_addr;
-    metaqueue_metadata[0].pointer=metaqueue_metadata[1].pointer=0;
+    data->metaqueue = (metaqueue_data*) data->uniq_shared_base_addr;
+    data->metaqueue_metadata[0].pointer=data->metaqueue_metadata[1].pointer=0;
 
 }
 __attribute__((constructor))
 void after_exec()
 {
+    pthread_key_create(&pthread_key, NULL);
     connect_monitor();
 }
 pid_t fork() {
@@ -46,18 +53,50 @@ void ipclib_sendmsg(int command, int data)
     data2send.data.command.data=data;
     //data2send.data.command.pid=gettid();
     metaqueue_pack q_pack;
-    q_pack.data = &metaqueue[0];
-    q_pack.meta = &metaqueue_metadata[0];
+    thread_data_t *t_data=NULL;
+    t_data=(thread_data_t *)pthread_getspecific(pthread_key);
+    q_pack.data = &(t_data->metaqueue[0]);
+    q_pack.meta = &(t_data->metaqueue_metadata[0]);
     metaqueue_push(q_pack, &data2send);
     DEBUG("Sent command %d to monitor", command);
 }
 void ipclib_recvmsg(metaqueue_element *data)
 {
     metaqueue_pack q_pack;
-    q_pack.data = &metaqueue[1];
-    q_pack.meta = &metaqueue_metadata[1];
+    thread_data_t *t_data=NULL;
+    t_data=(thread_data_t *)pthread_getspecific(pthread_key);
+    q_pack.data = &(t_data->metaqueue[1]);
+    q_pack.meta = &(t_data->metaqueue_metadata[1]);
     metaqueue_pop(q_pack, data);
     DEBUG("Recv from monitor");
 }
 #undef DEBUGON
 #define DEBUGON 0
+
+
+struct wrapper_arg {
+    void *(*func)(void *);
+    void *arg;
+};
+
+static void *wrapper(void *arg) {
+    struct wrapper_arg *warg = arg;
+    connect_monitor();
+    void *(*func)(void *) = warg->func;
+    arg = warg->arg;
+    free(warg);
+    pthread_exit(func(arg));
+    /* NORETURN */
+}
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+                   void *(*start_routine)(void *), void *arg) {
+    struct wrapper_arg *warg = malloc(sizeof(*warg));
+    if (!warg)
+        FATAL("malloc() failed");
+    warg->func = start_routine;
+    warg->arg = arg;
+    int result = ORIG(pthread_create, (thread, attr, wrapper, warg));
+    /* XXX */
+    return result;
+}
---
+++ test/test_pthread.c
@@ -0,0 +1,29 @@
+//
+// Created by ctyi on 11/23/17.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include "../lib/lib.h"
+#include <pthread.h>
+void* child_thread(void* arg)
+{
+    printf("Child thread!\n");
+    ipclib_sendmsg(REQ_PING, 8);
+    metaqueue_element data;
+    ipclib_recvmsg(&data);
+    printf("child back data%d\n", data.data.command.data);
+    return NULL;
+}
+int main()
+{
+    pthread_t thread;
+    pthread_create(&thread,NULL,child_thread,NULL);
+    printf("Parent process!");
+    ipclib_sendmsg(REQ_PING,16);
+    metaqueue_element data;
+    ipclib_recvmsg(&data);
+    printf("parent back data%d\n", data.data.command.data);
+    pthread_join(thread, NULL);
+    return 0;
+}
