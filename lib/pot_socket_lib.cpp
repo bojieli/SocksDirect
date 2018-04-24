ea4e29fd39675316f587e5cc9f8168ff006d380e
Tianyi Cui <cuity@cs.washington.edu>
2018-04-24 11:40:15 +0800

    Add test for paper intra-latency test

---
+++ lib/pot_socket_lib.cpp
@@ -0,0 +1,252 @@
+//
+// Created by ctyi on 4/23/18.
+//
+#include "../common/darray.hpp"
+#include "lib.h"
+#include "socket_lib.h"
+#include "../common/helper.h"
+#include "../common/interprocess_t.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstdarg>
+#include "lib_internal.h"
+#include "../common/metaqueue.h"
+#include <sys/ioctl.h>
+#include "fork.h"
+#include "pot_socket_lib.h"
+const int MAX_TST_MSG_SIZE=64*1024;
+static uint8_t pot_mock_data[MAX_TST_MSG_SIZE];
+
+void pot_init_write()
+{
+    for (int i=0;i<MAX_TST_MSG_SIZE;++i) pot_mock_data[i] = rand() % 256;
+}
+
+ssize_t pot_write_nbyte(int fd, int numofbytes)
+{
+    if (fd < FD_DELIMITER) return -1;
+    fd = MAX_FD_ID - fd;
+
+    thread_data_t *thread_data = GET_THREAD_DATA();
+    if (thread_data->fds_wr.begin(fd).end() || thread_data->fds_datawithrd[fd].type != USOCKET_TCP_CONNECT
+        || !thread_data->fds_datawithrd[fd].property.tcp.isopened)
+    {
+        errno = EBADF;
+        return -1;
+    }
+
+    monitor2proc_hook();
+
+    auto thread_sock_data = GET_THREAD_SOCK_DATA();
+    auto iter = thread_data->fds_wr.begin(fd);
+    interprocess_t *buffer = &thread_sock_data->
+            buffer[iter->buffer_idx].data;
+    int peer_fd = thread_data->fds_datawithrd[fd].peer_fd;
+    interprocess_t::queue_t::element ele;
+
+    if (numofbytes <= 16)
+    {
+        ele.command = interprocess_t::cmd ::NOP;
+        ele.pot_fd_rw.fd = peer_fd;
+        memcpy(ele.pot_fd_rw.raw, pot_mock_data, 9);
+        SW_BARRIER;
+        buffer->q[0].push(ele);
+        return 0;
+    }
+
+    if (numofbytes < 4096)
+    {
+        short startloc = buffer->b[0].pushdata(pot_mock_data, numofbytes);
+        ele.command = interprocess_t::cmd::DATA_TRANSFER;
+        ele.data_fd_rw.fd = peer_fd;
+        ele.data_fd_rw.pointer = startloc;
+        SW_BARRIER;
+        buffer->q[0].push(ele);
+        return 0;
+
+    }
+
+    if (numofbytes >= 4096)
+    {
+        FATAL("Zero copy not implemented!!!!!");
+    }
+    return 0;
+}
+
+enum ITERATE_FD_IN_BUFFER_STATE
+{
+    CLOSED,
+    FIND,
+    NOTFIND,
+    ALLCLOSED
+};
+
+static inline ITERATE_FD_IN_BUFFER_STATE recvfrom_iter_fd_in_buf
+        (int target_fd,
+         adjlist<file_struc_rd_t, MAX_FD_OWN_NUM, fd_rd_list_t, MAX_FD_PEER_NUM>::iterator& iter,
+         int &loc_in_buffer_has_blk, thread_data_t *thread_data, thread_sock_data_t *thread_sock_data)
+{
+
+    interprocess_t *buffer = &(thread_sock_data->buffer[iter->buffer_idx].data);
+    uint8_t pointer = buffer->q[1].tail;
+    bool islockrequired = (bool)(iter->status & FD_STATUS_RD_RECV_FORKED);
+    if (islockrequired)
+        pthread_mutex_lock(buffer->rd_mutex);
+    SW_BARRIER;
+    while (true)
+    {  //for same fd(buffer), iterate each available slot
+        interprocess_t::queue_t::element ele;
+        bool ele_isvalid, ele_isdel;
+        std::tie(ele_isvalid, ele_isdel) = buffer->q[1].peek(pointer, ele);
+        SW_BARRIER;
+        if (!ele_isvalid)
+            break;
+        if (!ele_isdel)
+        {
+            if (ele.command == interprocess_t::cmd::CLOSE_FD)
+            {
+                if (ele.close_fd.peer_fd == target_fd &&
+                    ele.close_fd.req_fd == thread_data->fds_datawithrd[target_fd].peer_fd)
+                {
+                    buffer->q[1].del(pointer);
+                    DEBUG("Received close req for %d from %d", ele.close_fd.peer_fd, ele.close_fd.req_fd);
+
+                    iter = thread_data->fds_datawithrd.del_element(iter);
+                    if (iter.end())
+                    {
+                        DEBUG("Destroyed self fd %d.", target_fd);
+                        thread_data->fds_datawithrd.del_key(target_fd);
+                        if (islockrequired)
+                            pthread_mutex_unlock(buffer->rd_mutex);
+                        return ITERATE_FD_IN_BUFFER_STATE::ALLCLOSED;
+                    }
+                    if (islockrequired)
+                        pthread_mutex_unlock(buffer->rd_mutex);
+                    return ITERATE_FD_IN_BUFFER_STATE::CLOSED; //no need to traverse this queue anyway
+                } else {
+                    if (!thread_data->fds_datawithrd.is_keyvalid(ele.close_fd.peer_fd))
+                        buffer->q[1].del(pointer);
+                }
+            }
+
+            if (ele.command == interprocess_t::cmd::DATA_TRANSFER &&
+                ele.data_fd_rw.fd == target_fd)
+            {
+                loc_in_buffer_has_blk = pointer;
+                if (islockrequired)
+                    pthread_mutex_unlock(buffer->rd_mutex);
+                return ITERATE_FD_IN_BUFFER_STATE::FIND;
+            }
+            if (ele.command == interprocess_t::cmd::NOP &&
+                    ele.pot_fd_rw.fd == target_fd)
+            {
+                loc_in_buffer_has_blk = pointer;
+                if (islockrequired)
+                    pthread_mutex_unlock(buffer->rd_mutex);
+                return ITERATE_FD_IN_BUFFER_STATE::FIND;
+            }
+        }
+        if (buffer->q[1].tail > pointer)
+            pointer = buffer->q[1].tail;
+        else
+            ++pointer;
+    }
+    iter = recv_empty_hook(iter, target_fd);
+    if (islockrequired)
+        pthread_mutex_unlock(buffer->rd_mutex);
+    return ITERATE_FD_IN_BUFFER_STATE::NOTFIND;
+}
+
+#undef DEBUGON
+#define DEBUGON 1
+
+#undef DEBUGON
+#define DEBUGON 0
+ssize_t pot_read_nbyte(int sockfd, void *buf, size_t len)
+{
+    //test whether fd exists
+    if (sockfd < FD_DELIMITER) FATAL("Invalid fd");
+    sockfd = MAX_FD_ID - sockfd;
+
+    thread_data_t *thread_data = GET_THREAD_DATA();
+    if (thread_data->fds_datawithrd.begin(sockfd).end() || thread_data->fds_datawithrd[sockfd].type != USOCKET_TCP_CONNECT
+        || !thread_data->fds_datawithrd[sockfd].property.tcp.isopened)
+    {
+        errno = EBADF;
+        return -1;
+    }
+
+    //hook for all the process
+    monitor2proc_hook();
+
+    auto thread_sock_data = GET_THREAD_SOCK_DATA();
+    interprocess_t *buffer_has_blk(nullptr);
+    int loc_has_blk(-1);
+    bool isFind(false);
+    do //if blocking infinate loop
+    {
+        auto iter = thread_data->fds_datawithrd.begin(sockfd);
+        while (true) //iterate different peer fd
+        {
+            int ret_loc(-1);
+            bool isFin(false);
+            interprocess_t *buffer = &(thread_sock_data->buffer[iter->buffer_idx].data);
+            ITERATE_FD_IN_BUFFER_STATE ret_state = recvfrom_iter_fd_in_buf(sockfd, iter, ret_loc, thread_data, thread_sock_data);
+            switch (ret_state)
+            {
+                case ITERATE_FD_IN_BUFFER_STATE::ALLCLOSED:
+                    return 0;
+                case ITERATE_FD_IN_BUFFER_STATE::CLOSED:
+                    break;
+                case ITERATE_FD_IN_BUFFER_STATE::FIND:
+                    buffer_has_blk = buffer;
+                    loc_has_blk = ret_loc;
+                    isFind=true;
+                    break;
+                case ITERATE_FD_IN_BUFFER_STATE::NOTFIND:
+                    if (iter.end())
+                        isFin=true;
+                    break;
+            }
+            if (isFin) break; //Finish iterate all peer fds
+            if (isFind) break; //Get the requested block
+        }
+        if (isFind) break;
+        monitor2proc_hook();
+    } while (thread_data->fds_datawithrd[sockfd].property.is_blocking);
+    int ret(len);
+    if (!isFind)
+    {
+        errno = EAGAIN | EWOULDBLOCK;
+        return -1;
+    } else {
+        //printf("found blk loc %d\n", loc_has_blk);
+        interprocess_t::queue_t::element ele;
+        buffer_has_blk->q[1].peek(loc_has_blk, ele);
+        if (ele.command == interprocess_t::cmd::DATA_TRANSFER)
+        {
+            short blk = buffer_has_blk->b[1].popdata(ele.data_fd_rw.pointer, ret, (uint8_t *) buf);
+            if (blk == -1)
+            {
+                SW_BARRIER;
+                buffer_has_blk->q[1].del(loc_has_blk);
+                SW_BARRIER;
+            } else
+            {
+                ele.data_fd_rw.pointer = blk;
+                buffer_has_blk->q[1].set(loc_has_blk, ele);
+            }
+        }
+        if (ele.command == interprocess_t::cmd::NOP)
+        {
+            memcpy(buf, ele.pot_fd_rw.raw, 9);
+            ret = 9;
+            SW_BARRIER;
+            buffer_has_blk->q[1].del(loc_has_blk);
+            SW_BARRIER;
+        }
+    }
+    return ret;
+}
+
---
+++ pot/eval_lat_c.c
@@ -0,0 +1,56 @@
+//
+// Created by ctyi on 11/30/17.
+//
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "../common/helper.h"
+#include "../lib/pot_socket_lib.h"
+uint8_t buffer[65536];
+long samples[10000];
+int main(int argc, char* argv[])
+{
+    if (argc < 3) FATAL("Lack of parameter: <output file name> <size of the message>");
+    int test_num=10000;
+    int inner_test_num = 1;
+    int test_size=atoi(argv[2]);
+    FILE* data_output_f = fopen(argv[1], "w");
+    pin_thread(0);
+
+    int fd;
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) FATAL("Failed to create fd");
+    struct sockaddr_in servaddr;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+    if (connect(fd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
+        FATAL("Failed to connect, %d", errno);
+    printf("connect succeed\n");
+
+    pot_init_write();
+    for (int i=0;i<test_num;++i)
+    {
+        //get time
+        struct timespec s_time, e_time;
+        clock_gettime(CLOCK_REALTIME, &s_time);
+
+        for (int j=0;j<inner_test_num; ++j)
+        {
+            //ping
+            pot_write_nbyte(fd, test_size);
+            //pong
+            pot_read_nbyte(fd, buffer, test_size);
+        }
+
+        //get time
+        clock_gettime(CLOCK_REALTIME, &e_time);
+
+        samples[i]= ((e_time.tv_sec - s_time.tv_sec) * (int)1e9 + (e_time.tv_nsec - s_time.tv_nsec)) / inner_test_num;
+    }
+
+    for (int i=0;i<test_num;++i) fprintf(data_output_f, "%ld ", samples[i]);
+    fclose(data_output_f);
+    return 0;
+}
