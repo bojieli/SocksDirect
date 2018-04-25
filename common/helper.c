4b81d30db040520cae9b4aea494ceed7eedce25b
Tianyi Cui <v-ticu@microsoft.com>
2018-04-25 22:34:20 +0800

    Add latency test for linux TCP

--- common/helper.c
+++ common/helper.c
@@ -1,7 +1,10 @@
 
 //some helper function
 #include "helper.h"
- 
+
+#define LOOP_CYCLE 100000000
+#define CAL_CYCLE 100000000
+volatile int tmp;
 
 pid_t gettid()
 {
@@ -19,55 +22,68 @@ int pin_thread(int core)
 /* assembly code to read the TSC */
 static inline uint64_t RDTSC()
 {
-  unsigned int hi, lo;
-  __asm__ volatile("rdtsc" : "=a" (lo), "=d" (hi));
-  return ((uint64_t)hi << 32) | lo;
+    unsigned int hi, lo;
+    __asm__ volatile("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((uint64_t) hi << 32) | lo;
 }
- 
+
 const int NANO_SECONDS_IN_SEC = 1000000000;
+
 /* returns a static buffer of struct timespec with the time difference of ts1 and ts2
    ts1 is assumed to be greater than ts2 */
 struct timespec *TimeSpecDiff(struct timespec *ts1, struct timespec *ts2)
 {
-  static struct timespec ts;
-  ts.tv_sec = ts1->tv_sec - ts2->tv_sec;
-  ts.tv_nsec = ts1->tv_nsec - ts2->tv_nsec;
-  if (ts.tv_nsec < 0) {
-    ts.tv_sec--;
-    ts.tv_nsec += NANO_SECONDS_IN_SEC;
-  }
-  return &ts;
+    static struct timespec ts;
+    ts.tv_sec = ts1->tv_sec - ts2->tv_sec;
+    ts.tv_nsec = ts1->tv_nsec - ts2->tv_nsec;
+    if (ts.tv_nsec < 0)
+    {
+        ts.tv_sec--;
+        ts.tv_nsec += NANO_SECONDS_IN_SEC;
+    }
+    return &ts;
 }
- 
+
 double g_TicksPerNanoSec;
+
 static void CalibrateTicks()
 {
-  struct timespec begints, endts;
-  uint64_t begin = 0, end = 0;
-  clock_gettime(CLOCK_MONOTONIC, &begints);
-  begin = RDTSC();
-  usleep(1000);
-  end = RDTSC();
-  clock_gettime(CLOCK_MONOTONIC, &endts);
-  struct timespec *tmpts = TimeSpecDiff(&endts, &begints);
-  uint64_t nsecElapsed = tmpts->tv_sec * 1000000000LL + tmpts->tv_nsec;
-  g_TicksPerNanoSec = (double)(end - begin)/(double)nsecElapsed;
+    struct timespec begints, endts;
+    for (int i = 0; i < LOOP_CYCLE; ++i)
+    {
+        __sync_synchronize();
+        tmp = i;
+    }
+    uint64_t begin = 0, end = 0;
+    clock_gettime(CLOCK_MONOTONIC, &begints);
+    begin = RDTSC();
+    for (int i = 0; i < CAL_CYCLE; ++i)
+    {
+        __sync_synchronize();
+        tmp = i;
+    }
+    end = RDTSC();
+    clock_gettime(CLOCK_MONOTONIC, &endts);
+    struct timespec *tmpts = TimeSpecDiff(&endts, &begints);
+    uint64_t nsecElapsed = tmpts->tv_sec * 1000000000LL + tmpts->tv_nsec;
+    g_TicksPerNanoSec = (double) (end - begin) / (double) nsecElapsed;
 }
- 
+
 /* Call once before using RDTSC, has side effect of binding process to CPU1 */
 void InitRdtsc()
 {
-  CalibrateTicks();
+    CalibrateTicks();
+    printf("Calibrated!");
 }
- 
+
 void GetTimeSpec(struct timespec *ts, uint64_t nsecs)
 {
-  ts->tv_sec = nsecs / NANO_SECONDS_IN_SEC;
-  ts->tv_nsec = nsecs % NANO_SECONDS_IN_SEC;
+    ts->tv_sec = nsecs / NANO_SECONDS_IN_SEC;
+    ts->tv_nsec = nsecs % NANO_SECONDS_IN_SEC;
 }
- 
+
 /* ts will be filled with time converted from TSC reading */
 void GetRdtscTime(struct timespec *ts)
 {
-  GetTimeSpec(ts, RDTSC() / g_TicksPerNanoSec);
+    GetTimeSpec(ts, RDTSC() / g_TicksPerNanoSec);
 }
