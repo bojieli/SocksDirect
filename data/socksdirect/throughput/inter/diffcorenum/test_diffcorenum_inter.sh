9a6aa569036ed8bc3e76cbac20966a3446fe8f1e
Bojie Li <v-bojli@microsoft.com>
2018-05-02 23:39:10 +0800

    fix diffconnnum scripts

--- data/socksdirect/throughput/inter/diffcorenum/test_diffcorenum_inter.sh
+++ data/socksdirect/throughput/inter/diffcorenum/test_diffcorenum_inter.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 ip="127.0.0.1"
 msgsize=8
-filename="inter.out"
-core=10
-for (( connnum=1; connnum<=1000000000; connnum*=10 ))
+for (( corenum=0; corenum<=15; corenum+=1 ))
 do
-    echo "Testing $connnum"
+    echo "Testing $corenum"
+    filename="intra-$(($corenum+1)).out"
     pkill pot
     ipcrm -a
     ssh -n 10.1.2.4 "pkill pot; ipcrm -a"
     ssh -n 10.1.2.4 "systemctl restart memcached"
-    HRD_REGISTRY_IP=10.1.2.4 ../../../../../build/pot_eval_rdma_tput_diffconnnum_s $msgsize $filename $core $connnum &
-    sleep 1
-    ssh -n -f 10.1.2.4 "cd /home/boj/libsd/build; HRD_REGISTRY_IP=10.1.2.4 nohup ./pot_eval_rdma_tput_diffconnnum_c $msgsize $filename $core $connnum &"
+    rm $filename
+    for (( core=0; core<=corenum; core+=1 ))
+    do
+        HRD_REGISTRY_IP=10.1.2.4 ../../../../../build/pot_eval_rdma_tput_s $msgsize $filename $core &
+    done
+    sleep 3
+    for (( core=0; core<=corenum; core+=1 ))
+    do
+        ssh -n -f 10.1.2.4 "cd /home/boj/libsd/build; HRD_REGISTRY_IP=10.1.2.4 nohup ./pot_eval_rdma_tput_c $msgsize $filename $core &"
+    done
     wait
 done
