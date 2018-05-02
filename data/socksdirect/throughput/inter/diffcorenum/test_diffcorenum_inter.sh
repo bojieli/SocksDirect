8c198a19247cd6b72a8d2272b336cdb409e818c4
Bojie Li <bojieli@gmail.com>
2018-05-02 23:26:39 +0800

    fix script

--- data/socksdirect/throughput/inter/diffcorenum/test_diffcorenum_inter.sh
+++ data/socksdirect/throughput/inter/diffcorenum/test_diffcorenum_inter.sh
@@ -10,7 +10,6 @@ do
     ipcrm -a
     ssh -n 10.1.2.4 "pkill pot; ipcrm -a"
     ssh -n 10.1.2.4 "systemctl restart memcached"
-    rm $filename
     HRD_REGISTRY_IP=10.1.2.4 ../../../../../build/pot_eval_rdma_tput_diffconnnum_s $msgsize $filename $core $connnum &
     sleep 1
     ssh -n -f 10.1.2.4 "cd /home/boj/libsd/build; HRD_REGISTRY_IP=10.1.2.4 nohup ./pot_eval_rdma_tput_diffconnnum_c $msgsize $filename $core $connnum &"
