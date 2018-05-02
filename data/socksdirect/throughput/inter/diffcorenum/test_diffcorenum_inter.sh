fad2c6153e93b4648ce92bc41ea2f7b9be361fda
Bojie Li <bojieli@gmail.com>
2018-05-02 23:26:01 +0800

    rdma diffconnnum

--- data/socksdirect/throughput/inter/diffcorenum/test_diffcorenum_inter.sh
+++ data/socksdirect/throughput/inter/diffcorenum/test_diffcorenum_inter.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 ip="127.0.0.1"
 msgsize=8
-for (( corenum=0; corenum<=15; corenum+=1 ))
+filename="inter.out"
+core=10
+for (( connnum=1; connnum<=1000000000; connnum*=10 ))
 do
-    echo "Testing $corenum"
-    filename="intra-$(($corenum+1)).out"
+    echo "Testing $connnum"
     pkill pot
     ipcrm -a
     ssh -n 10.1.2.4 "pkill pot; ipcrm -a"
     ssh -n 10.1.2.4 "systemctl restart memcached"
     rm $filename
-    for (( core=0; core<=corenum; core+=1 ))
-    do
-        HRD_REGISTRY_IP=10.1.2.4 ../../../../../build/pot_eval_rdma_tput_s $msgsize $filename $core &
-    done
-    sleep 3
-    for (( core=0; core<=corenum; core+=1 ))
-    do
-        ssh -n -f 10.1.2.4 "cd /home/boj/libsd/build; HRD_REGISTRY_IP=10.1.2.4 nohup ./pot_eval_rdma_tput_c $msgsize $filename $core &"
-    done
+    HRD_REGISTRY_IP=10.1.2.4 ../../../../../build/pot_eval_rdma_tput_diffconnnum_s $msgsize $filename $core $connnum &
+    sleep 1
+    ssh -n -f 10.1.2.4 "cd /home/boj/libsd/build; HRD_REGISTRY_IP=10.1.2.4 nohup ./pot_eval_rdma_tput_diffconnnum_c $msgsize $filename $core $connnum &"
     wait
 done
--- pot/eval_rdma_tput_diffconnnum_c.c
+++ pot/eval_rdma_tput_diffconnnum_c.c
@@ -8,30 +8,59 @@
 #include "../common/helper.h"
 #include "../lib/pot_socket_lib.h"
 uint8_t buffer[1048576]  __attribute__((aligned(PAGE_SIZE)));
-#define T1RND 10000000
 
 int main(int argc, char* argv[])
 {
+    int test_size = 8;
+    if (argc == 2)
+        test_size = atoi(argv[1]);
 
-    pin_thread(0);
+    int thread = 0;
+    if (argc >= 4)
+        thread = atoi(argv[3]);
 
-    int fd;
-    fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd == -1) FATAL("Failed to create fd");
-    struct sockaddr_in servaddr;
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(8080);
-    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
-    if (pot_connect(fd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
-        FATAL("Failed to connect, %d", errno);
+    pin_thread(thread);
+    
+    int conn_num = 1;
+    if (argc >= 5)
+        conn_num = atoi(argv[4]);
+
+    int *fd = (int *) malloc(sizeof(int) * conn_num);
+    if (fd == NULL) {
+        printf("failed to alloc mem\n");
+        return 1;
+    }
+    for (int i=0; i<conn_num; i++) {
+        fd[i] = socket(AF_INET, SOCK_STREAM, 0);
+        if (fd[i] == -1) FATAL("Failed to create fd");
+        struct sockaddr_in servaddr;
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(8080 + thread);
+        inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+        if (pot_connect(fd[i], (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
+            FATAL("Failed to connect, %d", errno);
+    }
     printf("connect succeed\n");
 
-    pot_init_write();
+    int test_num = 100000000;
+    if (test_size >= 512)
+        test_num /= 10;
+    if (test_size >= 131072)
+        test_num /= 10;
+
     TimingInit();
     InitRdtsc();
-    for (int i=0;i<3*T1RND;++i)
+
+    pot_init_write();
+    // warmup
+    for (int i=0;i<test_num;++i)
     {
-        pot_rdma_write_nbyte(fd, 8);
+        pot_rdma_write_nbyte(fd[i], test_size);
+    }
+
+    for (int i=0;i<test_num;++i)
+    {
+        pot_rdma_write_nbyte(fd[i], test_size);
     }
     return 0;
 }
