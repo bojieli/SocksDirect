f8d4679febb14dc4f880f33bf14359f0781e8da9
Bojie Li <bojieli@gmail.com>
2018-05-02 22:24:50 +0800

    diff core num script

---
+++ data/socksdirect/throughput/inter/diffcorenum/test_diffcorenum_inter.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+ip="127.0.0.1"
+msgsize=8
+for (( corenum=$1; corenum<=15; corenum+=1 ))
+do
+    echo "Testing $corenum"
+    filename="inter-$(($corenum+1)).out"
+    pkill pot
+    ipcrm -a
+    ssh -n 10.1.2.4 "pkill pot; ipcrm -a"
+    ssh -n 10.1.2.4 "systemctl restart memcached"
+    rm $filename
+    for (( core=0; core<=corenum; core+=1 ))
+    do
+        HRD_REGISTRY_IP=10.1.2.4 ../../../../../build/pot_eval_rdma_tput_s $msgsize $filename $(($core*2)) &
+    done
+    sleep 3
+    for (( core=0; core<=corenum; core+=1 ))
+    do
+        ssh -n -f 10.1.2.4 "cd /home/boj/libsd/build; HRD_REGISTRY_IP=10.1.2.4 nohup ./pot_eval_rdma_tput_c $msgsize $filename $core &"
+    done
+    wait
+done
