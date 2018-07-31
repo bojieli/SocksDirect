419f4aaf97ce7ab0d1f7e06b55c8bf663650c1d4
Tianyi Cui <cuity@cs.washington.edu>
2018-07-31 18:01:59 +0800

    Add QP Init

--- monitor/rdma_monitor.h
+++ monitor/rdma_monitor.h
@@ -8,5 +8,24 @@
 #define RDMA_SOCK_PORT 3000
 #define MAX_RDMA_CONN_NUM 32
 
+#include <infiniband/verbs.h>
+struct remote_process_t
+{
+    ibv_qp* myqp;
+    uint32_t rkey;
+    void* remote_buf_ptr;
+    int32_t qid;
+};
+
+struct qp_info_t
+{
+    uint16_t port_lid;
+    union ibv_gid RoCE_gid;
+    uint32_t qpn;
+    uint32_t buf_size;
+    uintptr_t remote_buf_addr;
+    uint32_t rkey;
+    int32_t qid;
+};
 void rdma_init();
 #endif //IPC_DIRECT_RDMA_H
