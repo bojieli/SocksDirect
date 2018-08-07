2929d10569e2b58a02acb05e264ef5929b4f5a78
Tianyi Cui <cuity@cs.washington.edu>
2018-08-07 20:42:58 +0800

    Put RDMA code to separate file

--- common/rdma.cpp
+++ common/rdma.cpp
@@ -86,3 +86,86 @@ void enum_dev(rdma_pack *p)
     assert(p->ib_ctx == nullptr);
     FATAL("Failed to enumerate device");
 }
+
+ibv_qp * rdma_create_qp(ibv_cq* cq, const rdma_pack * rdma_context)
+{
+    ibv_qp_init_attr myqp_attr;
+    myqp_attr.send_cq = cq;
+    myqp_attr.recv_cq = cq;
+    myqp_attr.qp_type = IBV_QPT_RC;
+    myqp_attr.cap.max_send_wr = QPSQDepth;
+    myqp_attr.cap.max_recv_wr = QPRQDepth;
+    myqp_attr.cap.max_send_sge = 1;
+    myqp_attr.cap.max_recv_sge = 1;
+    myqp_attr.cap.max_inline_data = QPMaxInlineData;
+    ibv_qp *qp;
+    qp=ibv_create_qp(rdma_context->ibv_pd, &myqp_attr);
+    if (qp == nullptr)
+        FATAL("Failed to create QP");
+    ibv_qp_attr myqp_stateupdate_attr;
+    memset(&myqp_stateupdate_attr, 0, sizeof(myqp_stateupdate_attr));
+    myqp_stateupdate_attr.qp_state = IBV_QPS_INIT;
+    myqp_stateupdate_attr.pkey_index = 0;
+    myqp_stateupdate_attr.port_num = rdma_context->dev_port_id;
+    myqp_stateupdate_attr.qp_access_flags =
+            IBV_ACCESS_REMOTE_READ |
+            IBV_ACCESS_REMOTE_WRITE |
+            IBV_ACCESS_REMOTE_ATOMIC;
+    if (ibv_modify_qp(qp, &myqp_stateupdate_attr,
+                      IBV_QP_STATE | IBV_QP_PKEY_INDEX | IBV_QP_PORT | IBV_QP_ACCESS_FLAGS))
+        FATAL("Failed to set QP to init");
+    return qp;
+}
+
+void rdma_connect_remote_qp(ibv_qp *qp, const rdma_pack * rdma_context, const qp_info_t * remote_qp_info)
+{
+
+    ibv_qp_attr myqp_stateupdate_attr;
+    //change state to RTR
+    memset(&myqp_stateupdate_attr, 0, sizeof(myqp_stateupdate_attr));
+    myqp_stateupdate_attr.qp_state = IBV_QPS_RTR;
+    myqp_stateupdate_attr.path_mtu = IBV_MTU_4096;
+    myqp_stateupdate_attr.dest_qp_num = remote_qp_info->qpn;
+    myqp_stateupdate_attr.rq_psn = 3185;
+
+    myqp_stateupdate_attr.ah_attr.is_global = 1;
+    myqp_stateupdate_attr.ah_attr.dlid = remote_qp_info->port_lid;
+    myqp_stateupdate_attr.ah_attr.sl = 0;
+    myqp_stateupdate_attr.ah_attr.src_path_bits = 0;
+    myqp_stateupdate_attr.ah_attr.port_num = rdma_context->dev_port_id;  // Local port!
+
+    auto& grh = myqp_stateupdate_attr.ah_attr.grh;
+    grh.dgid.global.interface_id = remote_qp_info->RoCE_gid.global.interface_id;
+    grh.dgid.global.subnet_prefix = remote_qp_info->RoCE_gid.global.subnet_prefix;
+    grh.sgid_index = 0;
+    grh.hop_limit = 1;
+
+    myqp_stateupdate_attr.max_dest_rd_atomic = 16;
+    myqp_stateupdate_attr.min_rnr_timer = 12;
+    int rtr_flags = IBV_QP_STATE | IBV_QP_AV | IBV_QP_PATH_MTU | IBV_QP_DEST_QPN |
+                    IBV_QP_RQ_PSN | IBV_QP_MAX_DEST_RD_ATOMIC | IBV_QP_MIN_RNR_TIMER;
+
+    if (ibv_modify_qp(qp, &myqp_stateupdate_attr, rtr_flags)) {
+        FATAL("Failed to modify QP from INIT to RTR");
+    }
+
+    //set QP to RTS
+    memset(&myqp_stateupdate_attr, 0, sizeof(myqp_stateupdate_attr));
+    myqp_stateupdate_attr.qp_state = IBV_QPS_RTS;
+    myqp_stateupdate_attr.sq_psn = 3185;
+
+    int rts_flags = IBV_QP_STATE | IBV_QP_SQ_PSN | IBV_QP_TIMEOUT | IBV_QP_RETRY_CNT | IBV_QP_RNR_RETRY |
+                    IBV_QP_MAX_QP_RD_ATOMIC;
+    myqp_stateupdate_attr.timeout = 14;
+    myqp_stateupdate_attr.retry_cnt = 7;
+    myqp_stateupdate_attr.rnr_retry = 7;
+    myqp_stateupdate_attr.max_rd_atomic = 16;
+    myqp_stateupdate_attr.max_dest_rd_atomic = 16;
+    rts_flags |= IBV_QP_TIMEOUT | IBV_QP_RETRY_CNT | IBV_QP_RNR_RETRY |
+                 IBV_QP_MAX_QP_RD_ATOMIC;
+
+
+    if (ibv_modify_qp(qp, &myqp_stateupdate_attr, rts_flags)) {
+        FATAL("Failed to modify QP from RTR to RTS");
+    }
+}
--- monitor/rdma_monitor.h
+++ monitor/rdma_monitor.h
@@ -9,23 +9,14 @@
 #define MAX_RDMA_CONN_NUM 32
 
 #include <infiniband/verbs.h>
+#include "../common/rdma.h"
 struct remote_process_t
 {
     ibv_qp* myqp;
     uint32_t rkey;
-    void* remote_buf_ptr;
+    intptr_t remote_buf_ptr;
     int32_t qid;
 };
 
-struct qp_info_t
-{
-    uint16_t port_lid;
-    union ibv_gid RoCE_gid;
-    uint32_t qpn;
-    uint32_t buf_size;
-    uintptr_t remote_buf_addr;
-    uint32_t rkey;
-    int32_t qid;
-};
 void rdma_init();
 #endif //IPC_DIRECT_RDMA_H
